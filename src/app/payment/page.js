09c0cca8578d3f7fde091d57607fa21dfac0be5e
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2024-11-22 21:20:01 +0100

    Updated message on no items in cart.

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -37,89 +37,18 @@ export default function Checkout({ searchParams }) {
         <FaArrowLeft />
         Back
       </Link>
-      <section className="place-self-center border border-slate-200 rounded-2xl p-12 grid gap-y-8">
+      <section className="place-self-center border border-slate-200 rounded-2xl p-12 grid gap-y-8 min-w-96">
         <h2 className="text-4xl font-bold cursor-default">Your Cart</h2>
-        <ProductCard
-          title={product.title}
-          img={color ? currentTeeColor.img.src : whiteTee}
-          size={size}
-          color={color}
-        />
-        {/* {size && color ? (
-          <article className="grid grid-cols-2 gap-6 max-w-96 py-4 border-t border-b border-slate-100 cursor-default">
-            <Image
-              src={color ? currentTeeColor.img.src : whiteTee}
-              alt={product.title}
-              width={600}
-              height={600}
-            />
-            <div>
-              <h3 className="text-2xl font-semibold mb-4">{product.title}</h3>
-              <p>
-                Size: <strong className="uppercase">{size}</strong>
-              </p>
-              <p>
-                Color: <strong>{color}</strong>
-              </p>
-            </div>
-          </article>
-        ) : size ? (
-          <article className="grid grid-cols-2 gap-6 max-w-96 py-4 border-t border-b border-slate-100 cursor-default">
-            <Image
-              src={currentTeeColor.img.src}
-              alt={product.title}
-              width={600}
-              height={600}
-            />
-            <div>
-              <h3 className="text-2xl font-semibold mb-4">{product.title}</h3>
-              <p>
-                Size: <strong className="uppercase">{size}</strong>
-              </p>
-              <p>
-                Color: <small>No color selected.</small>
-              </p>
-            </div>
-          </article>
-        ) : color ? (
-          <article className="grid grid-cols-2 gap-6 max-w-96 py-4 border-t border-b border-slate-100 cursor-default">
-            <Image
-              src={currentTeeColor.img.src}
-              alt={product.title}
-              width={600}
-              height={600}
-            />
-            <div>
-              <h3 className="text-2xl font-semibold mb-4">{product.title}</h3>
-              <p>
-                Size: <small>No size selected</small>.
-              </p>
-              <p>
-                Color: <strong>{color}</strong>
-              </p>
-            </div>
-          </article>
-        ) : (
-          "Please select size and color."
-        )} */}
-
-        {/* <article className="grid grid-cols-2 gap-6 max-w-96 py-4 border-t border-b border-slate-100 cursor-default">
-          <Image
-            src={currentTeeColor.img.src}
-            alt={product.title}
-            width={600}
-            height={600}
+        {size && color ? (
+          <ProductCard
+            title={product.title}
+            img={color ? currentTeeColor.img.src : whiteTee}
+            size={size}
+            color={color}
           />
-          <div>
-            <h3 className="text-2xl font-semibold mb-4">{product.title}</h3>
-            <p>
-              Size: <strong className="uppercase">{size}</strong>
-            </p>
-            <p>
-              Color: <strong>{color}</strong>
-            </p>
-          </div>
-        </article> */}
+        ) : (
+          <small>No items in cart.</small>
+        )}
         <button className="place-self-center inline-block w-36 text-slate-50 bg-green-800 hover:bg-green-700 rounded-md py-2 px-4">
           Pay now
         </button>
